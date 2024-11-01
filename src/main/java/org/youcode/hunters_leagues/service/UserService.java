09293e639cd45526d9caca80c10814e40f8ce816
FatimaEzahraeAdardor <adardorfatimaezahrae@gmail.com>
2024-11-01 11:01:20 +0100

    add update and delete

--- src/main/java/org/youcode/hunters_leagues/service/UserService.java
+++ src/main/java/org/youcode/hunters_leagues/service/UserService.java
@@ -2,7 +2,9 @@ package org.youcode.hunters_leagues.service;
 
 import org.youcode.hunters_leagues.domain.User;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface UserService {
     User save(User User);
@@ -10,4 +12,8 @@ public interface UserService {
     Optional<User> findByUserName(String username);
 
      Optional<User> login(User user);
+     User update(User user);
+     Boolean delete(UUID id);
+    List<User> findByUsernameOrEmail(String keyWord);
+
 }
