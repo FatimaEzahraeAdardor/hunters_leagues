fcab62f214609936d44e060dbf6ff58c2e1ff0cf
FatimaEzahraeAdardor <adardorfatimaezahrae@gmail.com>
2024-11-10 22:24:31 +0100

    crud species done

--- src/main/java/org/youcode/hunters_leagues/service/SpeicesService.java
+++
@@ -1,6 +0,0 @@
-package org.youcode.hunters_leagues.service;
-
-import org.apache.commons.lang3.ClassUtils;
-
-public interface SpeicesService {
-}
--- src/main/java/org/youcode/hunters_leagues/service/implementations/SpeicesServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package org.youcode.hunters_leagues.service.implementations;
-
-import org.youcode.hunters_leagues.service.SpeicesService;
-
-public class SpeicesServiceImpl implements SpeicesService {
-}
