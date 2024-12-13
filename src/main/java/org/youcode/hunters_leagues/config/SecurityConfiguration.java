1e01d72263ccd9b4cd9e0e94f8138fe66bcdd004
FatimaEzahraeAdardor <adardorfatimaezahrae@gmail.com>
2024-12-13 12:00:29 +0100

    implement https ssl

--- src/main/java/org/youcode/hunters_leagues/config/SecurityConfiguration.java
+++ src/main/java/org/youcode/hunters_leagues/config/SecurityConfiguration.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.youcode.hunters_leagues.filter.JwtAuthenticationFilter;
@@ -16,7 +18,6 @@ import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
 import static org.youcode.hunters_leagues.domain.enums.Permission.*;
 import static org.youcode.hunters_leagues.domain.enums.Role.*;
-
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
